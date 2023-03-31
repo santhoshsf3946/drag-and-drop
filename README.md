fa2c187dc372a01d03422c357c6a05a3d44989aa
santhoshsf3946 <115706419+santhoshsf3946@users.noreply.github.com>
2023-03-31 16:50:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# drag-and-drop
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/q2imr1)
---
+++ index.js
@@ -0,0 +1,20 @@
+var grid = new ej.grids.Grid({
+    dataSource: window.orderDatas,
+    allowRowDragAndDrop: true,
+    selectionSettings: { type: 'Multiple' },
+    height: 400,
+    allowPaging: true,
+    columns: [
+        { field: 'OrderID', headerText: 'Order ID', isPrimaryKey: true, width: 80, textAlign: 'Right' },
+        { field: 'CustomerName', headerText: 'Customer Name', width: 130, textAlign: 'Left' },
+        { field: 'OrderDate', headerText: 'Order Date', width: 120, format: 'yMd', textAlign: 'Right' },
+        { field: 'Freight', headerText: 'Freight', width: 130, format: 'C2', textAlign: 'Right' },
+        { field: 'ShipCity', headerText: 'Ship City', width: 130, textAlign: 'Left' },
+        { field: 'ShipCountry', headerText: 'Ship Country', width: 150 }
+    ]
+});
+grid.appendTo('#Grid');
+
+document.getElementById('resetReorderGrid').onclick = () => {
+    grid.freezeRefresh();
+}
